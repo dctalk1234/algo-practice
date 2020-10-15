8d0b516de8b651a31b79de35ba9f8b8ebdfc0215
Zane <zanebgriffin@gmail.com>
2020-10-15 17:44:58 -0500

    my solution to ugly number algo

---
+++ algos/javascript/uglyNumber/solution.js
@@ -0,0 +1,16 @@
+const isUgly = (num) => {
+  while (num !== 1){
+    if(num % 2 === 0){
+      num /= 2
+    } else if(num % 3 === 0) {
+      num /= 3
+    } else if(num % 5 === 0) {
+      num/= 5
+    } else {
+      return false
+    }
+   }
+  return true
+}
+
+console.log('is ugly', isUgly(9))

4d1edac21d810f5fdfdabf38932593eb9ec86fe0
Kenneth Cruz <37004078+knnyczr@users.noreply.github.com>
2020-10-20 10:32:36 -0400

    Create solution.js

---
+++ algos/javascript/dnaString/solution.js
@@ -0,0 +1,12 @@
+// let dnaTransform = (str) => {
+//   let dna = str.split("")
+//   dna.map((letter, index) => {
+//     if(letter == "A"){ dna.splice(index, 1, "T")}
+//     else if(letter == "T"){ dna.splice(index, 1, "A") }
+//     else if(letter == "C"){ dna.splice(index, 1, "G")}
+//     else if(letter == "G"){ dna.splice(index, 1, "C")}
+//   })
+//   return dna.join("")
+// }
+
+// dnaTransform(dnaString)
